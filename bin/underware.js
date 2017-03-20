6ebc7e86be97abdbd61f4cf06847a5c43b4480c7
Tyler Porter <porteta@gmail.com>
2017-03-20 13:13:32 -0700

    configure liftoff

--- bin/underware.js
+++ bin/underware.js
@@ -1,3 +1,39 @@
 #!/usr/bin/env node
 
-console.log("You're wearing underware");
+const Liftoff = require('liftoff');
+const chalk = require('chalk');
+const v8flags = require('v8flags');
+const argv = require('minimist')(process.argv.slice(2));
+const commands = argv._;
+
+const cli = new Liftoff({
+  name: 'underware',
+  configName: '.underware',
+  // completions: completion,
+  extensions: {
+    'rc': null
+  },
+  v8flags: v8flags,
+});
+
+// Exit with 0 or 1
+let failed = false;
+process.once('exit', function(code) {
+  if (code === 0 && failed) {
+    process.exit(1);
+  }
+});
+
+cli.launch({
+  cwd: argv.cwd,
+  configPath: argv.gulpfile,
+  require: argv.require,
+  completion: argv.completion,
+}, handleArguments);
+
+function handleArguments(env) {
+  if (commands.length === 0) {
+    console.log(chalk.green("You're wearing underware"));
+    process.exit(0);
+  }
+}
